14860ce23516f65bbd7ec39fb20de3be113cb9cc
firekatty <abhishekkatiyar1947@gmail.com>
2025-01-26 11:25:46 +0530

    Update connectDatabase.js

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -15,7 +15,7 @@
 
 const mongoose = require("mongoose");
 
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/KanbanBoard";
+const mongoUri = process.env.MONGO_URI;
 
 const connectToDatabase = async () => {
   try {
